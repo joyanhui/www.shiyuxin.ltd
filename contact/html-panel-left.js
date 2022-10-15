b9fc9ac72adaae9b38bf884144894a3ac5253dbf
joyanhui <40133116+joyanhui@users.noreply.github.com>
2022-10-16 07:52:23 +0800

    Delete contact directory

--- contact/html-panel-left.js
+++
@@ -1,7 +0,0 @@
-document.write("<p><a href=\"http:\/\/www.shiyuxin.ltd\" target=\"_blank\" external >网站首页<\/a><\/p>");
-document.write("<p><a href=\"http:\/\/www.shiyuxin.ltd\/app\" target=\"_blank\" external >app下载<\/a><\/p>");
-document.write("<p><a href=\"http:\/\/www.shiyuxin.ltd\/contact\/contact_kf.html\">联系方式<\/a><\/p>");
-document.write("<p><a href=\"http:\/\/www.shiyuxin.ltd\/contact\/join-In-Goup.html\">加群交流<\/a><\/p>");
-document.write("<p><\/p>");
-document.write("<p><a href=\"#\" class=\"close-panel\">× 关闭<\/a><\/p>");
-
